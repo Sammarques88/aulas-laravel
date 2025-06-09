3563a9c3c0b0a025a7fe23cb39ece37caee48186
SAMUEL MARQUES LUCIO <samuel.mlucio@senacsp.edu.br>
2025-06-09 11:42:50 -0300

    aula 1

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,3 @@ Route::get('/', function () {
 
 Route ::get('/sobrenos', function () { return 'Essa é a pagína sobre nós'; });
  
-Route ::get('/logincadastro', function () { return 'Essa é a pagína de login e cadastro'; });
-Route ::get('/menusalas', function () { return 'Essa é a pagína do menu de salas'; });
-Route ::get('/sala', function () { return 'Essa é a pagína da sala'; });
-Route ::get('/home', function () { return 'Essa é a pagína home'; });
-Route ::get('/contatos', function () { return 'Essa é a pagína sobre os contatos'; });
-Route ::get('/contrato', function () { return 'Essa é a pagína para contratar um profissional'; });
-Route::get('/login', function(){
-    return 'Essa é a página de login'; });
