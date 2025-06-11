c31c73bed8efb516214d465fb32eaa1b5e733ce3
Sammarques88 <sampah21.sp@gmail.com>
2025-06-11 10:49:29 -0300

    aula 11/06

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/index', function () {
-    return view('index');
+    return view('index'); 
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
